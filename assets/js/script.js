e46dea08d110e43ce7609df7a5b20abe09ea8a57
adjiepradana <adjiepradana01@gmail.com>
2026-05-03 11:12:08 +0100

    Fetch api form to spreadsheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxnhltgpos8_K9g9nI3B_R_Ty7qi5_AjS86Za9oJHuh6G135Iwn2WaCVFFucmAfTBYQyA/exec'
 
-const dataSiswa = function (nama, nisn, jenisKelamin, tanggalLahir, alamat, kelas, jurusan, noTelp, email) {
+const dataSiswa = function (
+    nama,
+    nisn,
+    jenisKelamin,
+    tanggalLahir,
+    alamat,
+    kelas,
+    jurusan,
+    noTelp,
+    email
+) {
 
     const siswa = {
         nama,
@@ -12,33 +23,92 @@ const dataSiswa = function (nama, nisn, jenisKelamin, tanggalLahir, alamat, kela
         noTelp,
         email
     }
+
     return siswa
 }
 
-const simpanData = function () {
+const simpanData = async function () {
+
+    const form = document.getElementById('formSiswa')
+
     const nama = document.getElementById('nama').value
-    const nisn = Number(document.getElementById('nisn').value)
+    const nisn = document.getElementById('nisn').value
     const jenisKelamin = document.getElementById('jenisKelamin').value
     const tanggalLahir = document.getElementById('tanggalLahir').value
     const alamat = document.getElementById('alamat').value
     const kelas = document.getElementById('kelas').value
     const jurusan = document.getElementById('jurusan').value
-    const noTelp = Number(document.getElementById('noTelp').value)
+    const noTelp = document.getElementById('noTelp').value
     const email = document.getElementById('email').value
 
-    if(!nama || !nisn || !jenisKelamin || !tanggalLahir || !alamat || !kelas || !jurusan || !noTelp || !email) {
+    if (
+        !nama ||
+        !nisn ||
+        !jenisKelamin ||
+        !tanggalLahir ||
+        !alamat ||
+        !kelas ||
+        !jurusan ||
+        !noTelp ||
+        !email
+    ) {
+
         Swal.fire({
             icon: 'warning',
             text: 'Lengkapi semua data!'
         })
+
         return
     }
 
-    const data = dataSiswa(nama, nisn, jenisKelamin, tanggalLahir, alamat, kelas, jurusan, noTelp, email)
+    const data = dataSiswa(
+        nama,
+        nisn,
+        jenisKelamin,
+        tanggalLahir,
+        alamat,
+        kelas,
+        jurusan,
+        noTelp,
+        email
+    )
+
     console.log(data)
 
-    Swal.fire({
-        icon: 'success',
-        text: 'Data berhasil disimpan!'
+    const formData = new FormData(form)
+
+    try {
+
+        Swal.fire({
+        title: 'Mengirim Data...',
+        text: 'Mohon tunggu sebentar',
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading()
+        }
     })
+
+        await fetch(scriptURL, {
+            method: 'POST',
+            body: formData
+        })
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Berhasil!',
+            text: 'Data siswa berhasil disimpan'
+        })
+
+        form.reset()
+
+    } catch(error) {
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Gagal!',
+            text: 'Data gagal dikirim'
+        })
+
+        console.log(error)
+    }
 }
